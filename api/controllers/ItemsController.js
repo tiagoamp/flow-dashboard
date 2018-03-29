c74072f7d39e5caa20bd131dd079bf40ab86add0
tiago.albuquerque <tiagoamp@>
2018-03-29 13:01:04 -0300

    Add database query with promises

--- api/controllers/ItemsController.js
+++ api/controllers/ItemsController.js
@@ -2,6 +2,7 @@ module.exports = function(app) {
 
     app.get('/items', function(req, res) {
         console.log('Teste');
+        console.log(new Date());
         res.send('OK');
     });
 
@@ -12,11 +13,38 @@ module.exports = function(app) {
         var connection = app.persistence.ConnectionFactory();
         var itemDao = new app.persistence.ItemDao(connection);
 
-        itemDao.save(item, function(err,result) {
-            console.log('Item created!');
-            res.json(item);
-        });
-        
+        let promise = itemDao.save(item);
+        promise
+            .then( (result) => {
+                console.log('Item created!');    
+                item.id = result.insertId;
+                
+                res.json(item);
+            } ) 
+            .catch( (err) => {
+                console.log("Failed: " + err);
+            } );       
+    });
+
+    app.post('/items/{id}', function(req,res) {
+        let itemHistory = req.body;
+        console.log('Request process for: ' + JSON.stringify(itemHistory));
+
+        var connection = app.persistence.ConnectionFactory();
+        var itemDao = new app.persistence.ItemDao(connection);
+
+        // TODO: SETAR ID NO HISTORY !!!!
+
+        let promise = itemDao.saveHistory(itemHistory);
+        promise
+            .then( (result) => {
+                console.log('Items history created!');    
+                
+                res.json(item);
+            } ) 
+            .catch( (err) => {
+                console.log("Failed: " + err);
+            } );       
     });
 
 }
--- api/persistence/ItemDao.js
+++ api/persistence/ItemDao.js
@@ -2,8 +2,18 @@ function ItemDao(connection) {
     this._connection = connection;
 }
 
-ItemDao.prototype.save = function(item,callback) {
-    this._connection.query('INSERT INTO ITEMS SET ?', item, callback);
+// ItemDao.prototype.save = function(item,callback) {
+//     this._connection.query('INSERT INTO ITEMS SET ?', item, callback);
+// }
+
+ItemDao.prototype.save = function(item) {
+    return new Promise( (resolve,reject) => {
+        this._connection.query('INSERT INTO ITEMS SET ?', item, function(err,result) {
+                if (err) reject("Database error: " + err);
+                resolve(result);
+            });
+        }
+    );    
 }
 
 ItemDao.prototype.findById = function (id,callback) {
@@ -12,6 +22,24 @@ ItemDao.prototype.findById = function (id,callback) {
 
 ItemDao.prototype.findAll = function () {
     this._connection.query("SELECT * FROM ITEMS", callback);
+}
+
+
+ItemDao.prototype.saveHistory = function(item) {
+    const historyWithId = item.statusHistory.map( (hist) => {
+        hist.id = item.id;
+        //console.log(hist);
+    });
+
+    //console.log(historyWithId);
+
+    return new Promise( (resolve,reject) => {
+        this._connection.query('INSERT INTO ITEMS_HISTORY SET ?', historyWithId, function(err,result) {
+                if (err) reject("Database error: " + err);
+                resolve(result);
+            });
+        }
+    );    
 }
 
 module.exports = function(){
