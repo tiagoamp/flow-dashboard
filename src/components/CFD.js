8f37488edca532f3b77aaa4954dcfd85e234fea5
tiagoamp <tiagoamp@gmail.com>
2020-01-23 14:59:33 -0300

    Remove commented code

--- src/components/CFD.js
+++ src/components/CFD.js
@@ -112,27 +112,11 @@ export default function CFD(props) {
     //console.log('matrix', historyMatrix);
     //console.log('by status', matrixByStatus);
     //console.log('cumulative', cumulativeMatrix);
-    console.log('dataSets', dataSets);
+    //console.log('dataSets', dataSets);
 
     const data = {
         labels: xLabels,
-        datasets: dataSets 
-        //labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],        
-        /*datasets: [
-          { 
-            data: [86,114,106,106,107,111,133,221,783,2478],
-            label: "Africa",
-            borderColor: "#3e95cd",
-            backgroundColor: "#3e95cd", 
-            fill: true
-          }, { 
-            data: [282,350,411,502,635,809,947,1402,3700,5267],
-            label: "Asia",
-            borderColor: "#8e5ea2",
-            backgroundColor: "#8e5ea2",
-            fill: true
-          }
-        ]*/        
+        datasets: dataSets            
     };
 
     return (
