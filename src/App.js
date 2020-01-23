28041856327bd8fc23c51bc4d796076d61cd9aa2
tiagoamp <tiagoamp@gmail.com>
2020-01-23 17:57:37 -0300

    Fix kanban: copy state objs params

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CFD from './components/CFD';
+import Burndown from './components/Burndown';
 import Kanban from './components/Kanban';
 import service from './service/flowservice';
 
@@ -10,12 +11,13 @@ class App extends Component {
 
   constructor() {
     super();
-    this.state = { projName: '', statusList: [], items: [] };        
+    this.state = { projName: '', statusList: [], items: [], holidays: [] };        
   }
 
   componentDidMount() {
     const info = service.getProjectInfo();
-    this.setState( {projName: info.project,  statusList: info.statusList, items: info.items } );
+    this.setState( {projName: info.project, holidays: info.holidays,   
+      statusList: info.statusList, items: info.items } );
   }
 
   render() {
@@ -23,12 +25,13 @@ class App extends Component {
       <div className="App">
         <Header project={this.state.projName} />
         <main className="container">
-          <CFD statusList={this.state.statusList} items={this.state.items} />
+          <CFD statusList={[...this.state.statusList]} items={[...this.state.items]} />
           {
-          //TO DO: <div>burndown</div>
-          //TO DO: <div>semáforo com cor e lead time (media) etc...</div>
+            //<Burndown items={[...this.state.items]} holidays={[...this.state.holidays]} />
+          
+            //TO DO: <div>semáforo com cor e lead time (media) etc...</div>
           }
-          <Kanban statusList={this.state.statusList} items={this.state.items} />
+          <Kanban statusList={[...this.state.statusList]} items={[...this.state.items]} />
         </main>
         <Footer />
       </div>
