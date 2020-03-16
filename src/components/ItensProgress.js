21e8c428cb276ef2356bfee780e057a95a6cd874
tiago.albuquerque <tiagoamp@>
2020-03-16 08:22:40 -0300

    Fix items delayed labels

--- src/components/ItensProgress.js
+++ src/components/ItensProgress.js
@@ -25,15 +25,23 @@ export default function ItensProgress(props) {
                     const perc = item.status === lastStatus ? 100 : (statusMult[item.status] * percentPerStatus);
                     const isDone = item.status === lastStatus;
                     const doneDate = isDone ? item.statusHistory.filter(h => h.status === lastStatus)[0].date : null;
-                    const isDelayed = isDone ? (new Date(doneDate).getTime() > new Date(item.dueDate).getTime()) : (new Date(item.dueDate).getTime() < today.getTime());
+                    const isDelayed = isDone ? (new Date(item.dueDate).getTime() < new Date(doneDate).getTime()) : (new Date(item.dueDate).getTime() < today.getTime());
+
+                    let daysDelayed = 0;
+                    if (isDelayed) {
+                        const finalDate = isDone ? new Date(doneDate) : today;
+                        daysDelayed = (finalDate.getTime() - new Date(item.dueDate).getTime()) / (1000 * 3600 * 24);
+                    } 
+                    
                     const isBlocked = item.blocked === "true";
+
                     return (
                         <div key={item.id}>
                             { isBlocked ? (<span className="progress-item-name" style={{color: "rgba(204, 102, 102, 0.7)"}}>{item.description}</span>) 
                                 : (<span className="progress-item-name">{item.description} </span>) 
                             }
                             { isDelayed ? 
-                                (<span className="progress-item-name" style={{color: "rgba(204, 102, 102, 0.7)"}}>(atrasou)</span>) 
+                                (<span className="progress-item-name" style={{color: "rgba(204, 102, 102, 0.7)"}}>({isDone ? (`atrasou ${daysDelayed} dias`) : 'atrasado'})</span>) 
                                 : ''}
                             { isBlocked ? 
                                 (<span className="progress-item-name" style={{color: "rgba(204, 102, 102, 0.7)", fontWeight: "bold"}}>(blocked)</span>) 
